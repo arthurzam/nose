af18d3b67eb33e3836704bab5a395e3307e46bbc
Arthur Zamarin <arthurzam@gentoo.org>
2021-11-11 10:34:32 +0200

    remove historical nosetests versioning

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ try:
         entry_points = {
         'console_scripts': [
             'nosetests = nose:run_exit',
-            'nosetests%s = nose:run_exit' % py_vers_tag,
             ],
         'distutils.commands': [
             ' nosetests = nose.commands:nosetests',
